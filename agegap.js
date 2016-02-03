69ed6d23ef3c8f79a3e5e3a656953ccfb6e3c0af
jonaslindstrom <jonas.lindstrom@hyperisland.se>
2016-02-03 15:10:57 +0100

    Jonas commitar

---
+++ agegap.js
@@ -0,0 +1,77 @@
+angular
+  .module('app', ['n3-line-chart'])
+          
+  .controller('MainCtrl', function($scope) {
+    $scope.data = {
+      dataset: [
+        {x: new Date("2015-01-01"), y: 0, val_2: 0, val_3: 0},
+        {x: new Date("2025-01-01"), y: 38113, val_2: -89588, val_3: 127701},
+        {x: new Date("2035-01-01"), y: 693191, val_2: -238239, val_3: 931430},
+        {x: new Date("2045-01-01"), y: 754199,val_2: -334779, val_3: 1088978},
+        
+       ]
+     };
+
+    $scope.options = {
+      axes: {
+        x: {
+          key: "x",
+            type: "date",
+           ticks: ("2015, 2025, 2035, 2045")
+        },
+          y: {
+              max: 1100000,
+              min: -400000,
+          },
+    
+      },
+        grid: {
+            x: true,
+            y: true,
+        },
+      tooltipHook: function(d){
+        return {
+          abscissas: "Population growth",
+          rows:  d.map(function(s){
+            return {
+              label: s.series.label,
+              value: s.row.y1,
+              color: s.series.color,
+              id: s.series.id  
+            }
+          })
+        }
+      },
+      series: [
+               {
+          dataset: "dataset",
+          key: 'y',
+          type: ['line', 'dot', 'area'],
+          label: 'None Workforce',
+          color: "rgb(235, 12, 12)",
+          interpolation: {mode: 'cardinal', tension: 0.7}
+        },
+        {
+          dataset: "dataset", 
+          key: 'val_2', 
+          label: 'Workforce', 
+          type: ['line', 'dot', 'line', 'area'],
+          color: "rgb(0, 0, 0)",
+          interpolation: {mode: 'cardinal', tension: 0.7}
+        },
+          {
+              dataset: "dataset",
+              key: 'val_3',
+              label: 'GAP',
+              color: "rgb(111, 111, 222)",
+              type: ['']
+          },
+    
+          
+          ]
+       
+    };
+ 
+  
+});
+    
---
+++ piechart2015.js
@@ -0,0 +1,101 @@
+
+zingchart.THEME="classic";
+
+var myConfig = {
+    "type":"pie",
+    "background-color":"#F2F2F0",
+    "title":{
+        "text":"2015",
+        "background-color":"none",
+        "color":"#333",
+
+    },
+    "plot":{
+        "slice":50,
+        "shadow":0,
+        "border-width":"5px",
+        "border-color":"#F2F2F0",
+        "value-box":{
+            "placement":"out",
+            "font-size":"10px",
+            "font-color":"#333"
+        },
+        "animation":{
+            "effect":"2",
+            "delay":"1000",
+            "speed":"500",
+            "method":"5",
+            "sequence":"1"
+        }
+    },
+    "tooltip":{
+        "fontSize":15,
+        "align":"left",
+        "borderRadius":3,
+        "borderWidth":2,
+        "borderColor":"%color-1",
+        "backgroundColor":"#fff",
+        "alpha":0.9,
+        "padding":10,
+        "color":"#000",
+        "negation":"currency",
+        "thousandsSeparator":",",
+        "shadow":0,
+                "caption": "2015",
+                "subCaption": "2015",
+                "paletteColors": "#0075c2,#1aaf5d,#f2c500,#f45b00,#8e0000",
+                "bgColor": "#ffffff",
+                "showBorder": "0",
+                "use3DLighting": "0",
+                "showShadow": "0",
+                "enableSmartLabels": "0",
+                "startingAngle": "0",
+                "showPercentValues": "1",
+                "showPercentInTooltip": "0",
+                "decimals": "1",
+                "captionFontSize": "14",
+                "subcaptionFontSize": "14",
+                "subcaptionFontBold": "0",
+                "toolTipColor": "#ffffff",
+                "toolTipBorderThickness": "0",
+                "toolTipBgColor": "#000000",
+                "toolTipBgAlpha": "80",
+                "toolTipBorderRadius": "2",
+                "toolTipPadding": "5",
+                "showHoverEffect":"1",
+                "showLegend": "1",
+                "legendBgColor": "#ffffff",
+                "legendBorderAlpha": '0',
+                "legendShadow": '0',
+                "legendItemFontSize": '10',
+                "legendItemFontColor": '#666666',
+        "text":"<b style=\"color:%color\">%negation%node-value</b> people"
+    },
+    "series":[
+        {
+            "text":"Children 0-19"+"<br>" + "22,7%",
+            "values":[2210796],
+            "background-color":"#CE8580"
+        },
+        {
+            "text":"Workforce 20-63" + "<br>" + "56,5%",
+            "values":[5505470],
+            "background-color":"#C4473F"
+        },
+        {
+            "text":"Elderly 64+" +"<br>"+"20,8%",
+            "values":[2027361],
+            "background-color":"#442C2B"
+        }
+    ]
+};
+
+
+
+
+zingchart.render({ 
+	id : 'chart1', 
+	data : myConfig, 
+	height: 300, 
+	width: 300 
+});
